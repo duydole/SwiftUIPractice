16ef7da38a10a3954bbbd1e252020ac93b47dc12
Do Le Duy <>
2020-04-30 23:18:32 +0700

    duydl - Done DynamicList

--- DynamicList/DynamicList/ContentView.swift
+++ DynamicList/DynamicList/ContentView.swift
@@ -8,14 +8,60 @@
 
 import SwiftUI
 
+// User must conform Identifiable protocol to use with List
+struct User : Identifiable {
+    let id = UUID()
+    let username, message, imagename: String
+}
+
 struct ContentView: View {
+    
+    private let users: [User] = [
+        .init(username: "Tim Cook", message: "My nice shiny new monitor stand is $999", imagename: "1"),
+        .init(username: "Craig Federighi", message: "My nice shiny new monitor stand is $999",imagename: "2"),
+        .init(username: "Jon Ivey", message: "My nice shiny new monitor stand is $999", imagename: "3"),
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                // Header
+                Section {Text("Users").font(.largeTitle)}
+                
+                // Body
+                Section {
+                    ForEach (self.users) { user in
+                        UserRow(user: user)
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Dynamic List"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct UserRow: View {
+    var user: User
+    var body: some View {
+        HStack {
+            Image(user.imagename)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.black, lineWidth: 2))
+            
+            
+            VStack (alignment: .leading) {
+                Text(user.username).font(.headline)
+                Text(user.message).font(.subheadline)
+            }
+        }.padding(.all, 5)
     }
 }
