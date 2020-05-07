687229145fd849cadaec5433ab004fd69995f58f
Do Le Duy <>
2020-05-07 21:09:53 +0700

    duydl - Demo Tabbar init commit

---
+++ TabbarDemo/TabbarDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabbarDemo
+//
+//  Created by Do Le Duy on 5/7/20.
+//  Copyright © 2020 Do Le Duy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
